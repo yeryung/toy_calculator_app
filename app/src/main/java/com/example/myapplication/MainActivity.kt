3d4888d163a992e9c36727c2d231fe45970c1318
ery270 <ery270@naver.com>
2022-08-27 16:47:09 +0900

    commit calculator app files

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+    fun onDigit(view: View){
+        Toast.makeText(this,"Button clicked",Toast.LENGTH_LONG).show()
+    }
+}
